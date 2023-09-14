8af7a7af7713ccf4c1d6598511f5f26fdcc15575
kbthebee <ilikebeeswarm@gmail.com>
2023-09-14 22:51:33 +0800

    commiting ae2 wip script

--- kubejs/client_scripts/hideJEI.js
+++
@@ -1,5 +0,0 @@
-JEIEvents.hideItems(event => {
-
-
-
-})
--- kubejs/server_scripts/unification/tags.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags('item', event => {
-    event.add('forge:ingots/neutronium', 'avaritia:neutron_ingot')
-    event.add('forge:nuggets/neutronium', 'avaritia:neutron_nugget')
-})
